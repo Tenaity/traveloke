b247b73391eb639849bba39c54ce7d0f4859f3eb
Tenaity <tenaiti.150320@gmail.com>
2021-09-25 05:41:55 +0700

    update UI + form search hotel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Articles from "./components/Articles";
 const colors = {
   brand: {
     900: "#1a365d",
@@ -13,13 +14,13 @@ const colors = {
 
 const theme = extendTheme({ colors });
 
-// 3. Pass the `theme` prop to the `ChakraProvider`
 const App = ({ Component }) => {
   return (
     <ChakraProvider theme={theme}>
       {/* <Component /> */}
       <Navbar />
       <Hero />
+      <Articles />
       <Footer />
     </ChakraProvider>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,106 +1,194 @@
 import {
   Box,
-  Heading,
-  Container,
-  Text,
-  Button,
-  Stack,
-  Icon,
   useColorModeValue,
-  createIcon,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Text,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+  Select,
+  Button,
 } from "@chakra-ui/react";
-
+import {
+  IoBusOutline,
+  IoAirplaneOutline,
+  IoBusinessOutline,
+  IoTrainOutline,
+  IoCarSportOutline,
+  IoCogOutline,
+  IoLocationOutline,
+  IoCalendarOutline,
+  IoMoonOutline,
+  IoCaretDownOutline,
+  IoManOutline,
+  IoReceiptOutline,
+} from "react-icons/io5";
 const Hero = () => {
+  const bg = useColorModeValue("white", "gray.800");
   return (
-    <>
-      {/* <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head> */}
-
-      <Container maxW={"3xl"}>
-        <Stack
-          as={Box}
-          textAlign={"center"}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}
+    <Box w="100vw" h="100vh">
+      <Box
+        w="100vw"
+        h="50vh"
+        bgImage={
+          bg === "white"
+            ? "url('../../images/beach.jpg')"
+            : "url('../../images/darkBeach.jpg')"
+        }
+        bgRepeat="no-repeat"
+        bgPos="top"
+        pos="relative"
+        d="flex"
+        justifyContent="center"
+      >
+        <Box
+          pos="absolute"
+          bg={bg}
+          borderRadius="xl"
+          w="800px"
+          h="400"
+          top="305"
+          boxShadow="2xl"
+          overflow="hidden"
         >
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-            lineHeight={"110%"}
-          >
-            Make money from <br />
-            <Text as={"span"} color={"green.400"}>
-              your audience
-            </Text>
-          </Heading>
-          <Text color={"gray.500"}>
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
-          </Text>
-          <Stack
-            direction={"column"}
-            spacing={3}
-            align={"center"}
-            alignSelf={"center"}
-            position={"relative"}
-          >
-            <Button
-              colorScheme={"green"}
-              bg={"green.400"}
-              rounded={"full"}
-              px={6}
-              _hover={{
-                bg: "green.500",
-              }}
-            >
-              Get Started
-            </Button>
-            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
-              Learn more
-            </Button>
-            <Box>
-              <Icon
-                as={Arrow}
-                color={useColorModeValue("gray.800", "gray.300")}
-                w={71}
-                position={"absolute"}
-                right={-71}
-                top={"10px"}
-              />
-              <Text
-                fontSize={"lg"}
-                fontFamily={"Caveat"}
-                position={"absolute"}
-                right={"-125px"}
-                top={"-15px"}
-                transform={"rotate(10deg)"}
+          <Box>
+            <Tabs defaultIndex={0} borderBottomColor="transparent">
+              <TabList
+                d="flex"
+                justifyContent="center"
+                bg={useColorModeValue("gray.200", "gray.600")}
               >
-                Starting at $15/mo
-              </Text>
-            </Box>
-          </Stack>
-        </Stack>
-      </Container>
-    </>
+                <Tab py={4} m={0} _focus={{ boxShadow: "none" }}>
+                  <IoBusinessOutline />
+                  <Text ml="5px">Hotel</Text>
+                </Tab>
+                <Tab py={4} m={0} _focus={{ boxShadow: "none" }}>
+                  <IoTrainOutline /> <Text ml="5px">Train</Text>
+                </Tab>
+                <Tab py={4} m={0} _focus={{ boxShadow: "none" }}>
+                  <IoCarSportOutline /> <Text ml="5px">Car</Text>
+                </Tab>
+                <Tab py={4} m={0} _focus={{ boxShadow: "none" }}>
+                  <IoAirplaneOutline /> <Text ml="5px">Plane</Text>
+                </Tab>
+                <Tab py={4} m={0} _focus={{ boxShadow: "none" }}>
+                  <IoBusOutline /> <Text ml="5px">Bus</Text>
+                </Tab>{" "}
+                <Tab isDisabled py={4} m={0}>
+                  <IoCogOutline /> <Text ml="5px">Setting</Text>
+                </Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <Stack>
+                    <Box>
+                      <Text mb="2">Thành phố địa điểm hoặc tên khách sạn:</Text>
+                      <InputGroup>
+                        <InputLeftElement
+                          pointerEvents="none"
+                          children={<IoLocationOutline />}
+                        />
+                        <Input
+                          type="tel"
+                          placeholder="Đà Nẵng"
+                          _focus={{ boxShadow: "1px none" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box d="flex">
+                      <Box mr="25px">
+                        <Text mb="2">Nhận phòng:</Text>
+                        <InputGroup>
+                          <InputLeftElement
+                            pointerEvents="none"
+                            children={<IoCalendarOutline />}
+                          />
+                          <Input
+                            type="tel"
+                            placeholder="Đặt phòng"
+                            _focus={{ boxShadow: "1px none" }}
+                          />
+                        </InputGroup>
+                      </Box>
+                      <Box mr="25px">
+                        <Text mb="2">Số đêm:</Text>
+                        <InputGroup>
+                          <InputLeftElement
+                            pointerEvents="none"
+                            children={<IoMoonOutline />}
+                          />
+                          <Input
+                            as="select"
+                            placeholder="Số đêm ở tại khách sạn"
+                            w="250px"
+                            _focus={{ boxShadow: "none" }}
+                            icon={<IoCaretDownOutline />}
+                          >
+                            <option value="option1">1 Đêm</option>
+                            <option value="option1">2 Đêm</option>
+                            <option value="option1">3 Đêm</option>
+                            <option value="option1">4 Đêm</option>
+                            <option value="option1">5 Đêm</option>
+                            <option value="option1">6 Đêm</option>
+                          </Input>
+                        </InputGroup>
+                      </Box>
+                      <Box>
+                        <Text mb="2">Trả phòng:</Text>
+                        <Text mt="4">Thứ 5, 15 thg 3 2022</Text>
+                      </Box>
+                    </Box>
+                    <Box>
+                      <Text mb="2">Khách và phòng:</Text>
+                      <InputGroup>
+                        <InputLeftElement
+                          pointerEvents="none"
+                          children={<IoManOutline />}
+                        />
+                        <Input
+                          type="tel"
+                          placeholder="1 người lớn, 0 trẻ em, 1 phòng"
+                          _focus={{ boxShadow: "1px none" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box d="flex" alignItems="baseline">
+                      <IoReceiptOutline />
+                      <Text as="button" align="left" ml="10px">
+                        Thanh toán khi nhận phòng
+                      </Text>
+                    </Box>
+                    <Box d="flex" justifyContent="center">
+                      <Button
+                        w="300px"
+                        color="white"
+                        bg={useColorModeValue("blue.500", "blue.700")}
+                        _hover={{
+                          bg: useColorModeValue("blue.300", "blue.500"),
+                        }}
+                        _focus={{ boxShadow: "none" }}
+                      >
+                        Tìm khách sạn
+                      </Button>
+                    </Box>
+                  </Stack>
+                </TabPanel>
+                <TabPanel>Train</TabPanel>
+                <TabPanel>Car</TabPanel>
+                <TabPanel>Plane</TabPanel>
+                <TabPanel>Bus</TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
-
-const Arrow = createIcon({
-  displayName: "Arrow",
-  viewBox: "0 0 72 24",
-  path: (
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M0.600904 7.08166C0.764293 6.8879 1.01492 6.79004 1.26654 6.82177C2.83216 7.01918 5.20326 7.24581 7.54543 7.23964C9.92491 7.23338 12.1351 6.98464 13.4704 6.32142C13.84 6.13785 14.2885 6.28805 14.4722 6.65692C14.6559 7.02578 14.5052 7.47362 14.1356 7.6572C12.4625 8.48822 9.94063 8.72541 7.54852 8.7317C5.67514 8.73663 3.79547 8.5985 2.29921 8.44247C2.80955 9.59638 3.50943 10.6396 4.24665 11.7384C4.39435 11.9585 4.54354 12.1809 4.69301 12.4068C5.79543 14.0733 6.88128 15.8995 7.1179 18.2636C7.15893 18.6735 6.85928 19.0393 6.4486 19.0805C6.03792 19.1217 5.67174 18.8227 5.6307 18.4128C5.43271 16.4346 4.52957 14.868 3.4457 13.2296C3.3058 13.0181 3.16221 12.8046 3.01684 12.5885C2.05899 11.1646 1.02372 9.62564 0.457909 7.78069C0.383671 7.53862 0.437515 7.27541 0.600904 7.08166ZM5.52039 10.2248C5.77662 9.90161 6.24663 9.84687 6.57018 10.1025C16.4834 17.9344 29.9158 22.4064 42.0781 21.4773C54.1988 20.5514 65.0339 14.2748 69.9746 0.584299C70.1145 0.196597 70.5427 -0.0046455 70.931 0.134813C71.3193 0.274276 71.5206 0.70162 71.3807 1.08932C66.2105 15.4159 54.8056 22.0014 42.1913 22.965C29.6185 23.9254 15.8207 19.3142 5.64226 11.2727C5.31871 11.0171 5.26415 10.5479 5.52039 10.2248Z"
-      fill="currentColor"
-    />
-  ),
-});
 
 export default Hero;
